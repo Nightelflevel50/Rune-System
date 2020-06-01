4e3fc35e7e9c6780600cde689b1d563bd99082a4
Nightelflevel50 <55340964+Nightelflevel50@users.noreply.github.com>
2020-06-01 01:53:07 -0400

    Update RangeAlgorithm.java

--- RangeAlgorithm.java
+++ RangeAlgorithm.java
@@ -21,12 +21,35 @@ public class RangeAlgorithm {
 		int green = newRune.getGreen(x, y);
 
 		while (x > (0 + green) & y > (0 + green)) {
-			x = x - green;
-			y = y - green;
+			//This is for diagonal upper left
+			System.out.printf("Range UL %d %d\n", x, y);
+			int temp_x = x - green;
+			int temp_y = y - green;
+			if (newRune.isWhite(temp_x, temp_y)) {
+				System.out.printf("Range it returned white\n");
+				break;
+			}
 			counterIncrease();
+			x = temp_x;
+			y = temp_y;
 			green = newRune.getGreen(x, y);
 		}
-
+		
+		while (x < (299 - green) & y > green) {
+			//This is for diagonal upper right
+			System.out.printf("Range UR %d %d\n", x, y);
+			int temp_x = x - green;
+			int temp_y = y - green;
+			if (newRune.isWhite(temp_x, temp_y)) {
+				System.out.printf("Range it returned white\n");
+				break;
+			}
+			counterIncrease();
+			x = temp_x;
+			y = temp_y;
+			green = newRune.getGreen(x, y);
+		}
+		
 		cords.setLocation(x, y);
 
 		return cords;
