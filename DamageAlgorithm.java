7383ab8169748f2e5091cd482744b3794efe74f7
Nightelflevel50 <55340964+Nightelflevel50@users.noreply.github.com>
2020-06-01 01:47:57 -0400

    Update DamageAlgorithm.java

--- DamageAlgorithm.java
+++ DamageAlgorithm.java
@@ -21,12 +21,47 @@ public class DamageAlgorithm {
 		int red = newRune.getRed(x, y);
 
 		while (x < (299 - red) & y < (299 - red)) {
-			x = x + red;
-			y = y + red;
+			// This is for diagonal bottom right
+			System.out.printf("Damage BR %d %d\n", x, y);
+			int temp_x = x + red;
+			int temp_y = y + red;
+			if (newRune.isWhite(temp_x, temp_y)) {
+				System.out.printf("poopy it returned white\n");
+				break;
+			}
 			counterIncrease();
+			x = temp_x;
+			y = temp_y;
 			red = newRune.getRed(x, y);
 		}
-		
+
+		while (x > red & y < (299 - red)) {
+			// This is for diagonal bottom left
+			System.out.printf("Damage BL %d %d\n", x, y);
+			int temp_x = x - red;
+			int temp_y = y + red;
+			if (newRune.isWhite(temp_x, temp_y)) {
+				System.out.printf("poopy it returned white\n");
+				break;
+			}
+			counterIncrease();
+			x = temp_x;
+			y = temp_y;
+			red = newRune.getRed(x, y);
+		}
+
+		/* These are for upper left and upper right, I am not sure if I want to use these yet
+		 * 
+		 * while (x > red & y > red) {
+		 * 
+		 * // This is for diagonal upper left x = x - red; y = y - red;
+		 * counterIncrease(); red = newRune.getRed(x, y); }
+		 * 
+		 * while (x < (299 - red) & y > red) { // This is for diagonal upper right
+		 * System.out.printf("%d %d\n", x, y); x = x + red; y = y - red;
+		 * counterIncrease(); red = newRune.getRed(x, y); }
+		 */
+
 		cords.setLocation(x, y);
 
 		return cords;
@@ -41,3 +76,4 @@ public class DamageAlgorithm {
 	}
 
 }
+
